dbb53f4e54519a7fd7897710b34a27d882d97fbd
JeonJungyu-1 <85854928+JeonJungyu-1@users.noreply.github.com>
2021-10-13 09:50:00 +0900

    Delete mybatis directory

--- mybatis/Mybatis_Student/src/student/ui/StudentMain.java
+++
@@ -1,9 +0,0 @@
-package student.ui;
-
-public class StudentMain {
-
-	public static void main(String[] args) {
-		StudentUI ui = new StudentUI();
-	}
-
-}
--- mybatis/Test1st_ShopProduct/src/shop/ui/ShopMain.java
+++
@@ -1,8 +0,0 @@
-package shop.ui;
-
-public class ShopMain {
-	
-	public static void main(String[] args) {
-		new ShopUI();
-	}
-}
